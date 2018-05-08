bd4ed12e3b949c554414b6401f58bb10a304d1d1
Anna <anna@anna-virtual-machine>
2018-05-08 01:27:14 -0500

    reverting to in class prog check

--- world.py
+++ world.py
@@ -10,12 +10,31 @@ def index():
 	res = db.query('select distinct continent from country')
 	return render_template('index.html', res=res)
 
-@app.route('/procForm')
+@app.route('/procContForm')
 def getData():
 	db = records.Database('postgresql://rublan01:@localhost:2345/world')
 	continent = request.args.get('cont')
 	contCountries = db.query('select * from country where continent=:continent', continent=continent)
-	return render_template('data.html', continent=continent, contCountries=contCountries)
+	return render_template('data.html', continent=continent, contCountries=contCountries), continent
+
+@app.route('/procCountry')
+def country():
+	db = records.Database('postgresql://rublan01:@localhost:2345/world')
+	country = request.args.get('country')
+	continent = getData()
+	countryList = ('select distinct name from country where continent=:continent')
+	return render_template('country.html')
+
+
+@app.route('/procLang')
+def language():
+	db = records.Database('postgresql://rublan01:@localhost:2345/world')
+
+
+@app.route('/procCity')
+def city():
+	db = records.Database('postgresql://rublan01:@localhost:2345/world')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
